4d1c18e54b4527e39629ec97cb1e7f79917f3804
Cyberxt1 <gmiacx67@gmail.com>
2025-06-11 01:57:31 +0100

    Change Echo

--- user/user.js
+++ user/user.js
@@ -20,7 +20,16 @@ import {
 } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-firestore.js";
 
 // --- Firebase Setup ---
-const firebaseConfig = { /* your config */ }; // <-- fill this in
+const firebaseConfig = {
+  apiKey: "AIzaSyB9aIZfqZvtfOSNUHGRSDXMyWDxWWS5NNs",
+  authDomain: "techfix-ef115.firebaseapp.com",
+  projectId: "techfix-ef115",
+  storageBucket: "techfix-ef115.firebasestorage.app",
+  messagingSenderId: "798114675752",
+  appId: "1:798114675752:web:33450b57585b4a643b891d",
+  measurementId: "G-69MKL5ETH7"
+};
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
